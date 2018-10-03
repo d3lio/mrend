609cfcb30930e687cd7865c7fa9fb370ee384a25
Delyan Dobrev <delqn.dobrev@gmail.com>
2018-10-03 23:32:55 +0300

    feature(slides): Improve wrapping slides

--- src/plugins/wrapping-pages/index.js
+++ src/plugins/wrapping-pages/index.js
@@ -1,11 +1,18 @@
-module.exports = metadata => ({
-    phase: 'extend',
-    resources: ['wrapping-pages.css'],
-    run(slides) {
-        const desc = metadata.description ? `\n### ${metadata.description}` : '';
-        const date = metadata.date ? `\n### ${metadata.date}` : '';
-        slides.unshift(`# ${metadata.title}${desc}${date}`);
-        slides.push('# The end');
-        return slides;
-    },
-});
+module.exports = metadata => {
+    const LOCALES = require('./locales.json');
+    const qnaLocalizedDefault = (LOCALES[metadata.lang] || {}).qna;
+
+    const desc = metadata.description ? `\n### ${metadata.description}` : '';
+    const date = metadata.date ? `\n### ${metadata.date}` : '';
+    const qna = metadata['wrapping-qna'] || qnaLocalizedDefault || 'Q&A';
+
+    return {
+        phase: 'extend',
+        resources: ['wrapping-pages.css'],
+        run(slides) {
+            slides.unshift(`# ${metadata.title}${desc}${date}`);
+            slides.push(`# ${qna}`);
+            return slides;
+        },
+    };
+};
