456f0be9899109dc153359100f6ce10f60af35f5
Delyan Dobrev <delqn.dobrev@gmail.com>
2018-10-10 14:09:51 +0300

    feature(watcher): Watch and rebuild md on change with -w flag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const timer = process.hrtime();
+let timer = process.hrtime();
 
 const fs = require('fs-extra');
 const path = require('path');
@@ -19,6 +19,7 @@ const optionsDefinitions = [
     { name: 'help', alias: 'h', type: Boolean },
     { name: 'input', alias: 'i', type: filename => new FileDetails(filename), defaultOption: true },
     { name: 'output', alias: 'o', type: String, defaultValue: 'output' },
+    { name: 'watch', alias: 'w', type: Boolean },
 ];
 
 const options = commandLineArgs(optionsDefinitions);
@@ -39,8 +40,6 @@ if (!options.input.exists) {
     process.exit(1);
 }
 
-const p = path.join.bind(path);
-
 const input = options.input.filename;
 
 console.info('markdown:', input);
@@ -52,35 +51,39 @@ function createSlide(content, metadata) {
     };
 }
 
-let slides = fs.readFileSync(input, 'UTF8')
-    .split(/^\s*?---\s*?\n/gm)
-    .slice(1)
-    .map(content => createSlide(content));
+let slides = parseInput();
 
-console.info('parsing metadata');
+function parseInput() {
+    return fs.readFileSync(input, 'UTF8')
+        .split(/^\s*?---\s*?\n/gm)
+        .slice(1)
+        .map(content => createSlide(content));
+}
 
-const metadata = (function parseMetadata() {
-    const metadataConverter = new showdown.Converter({ metadata: true });
-    metadataConverter.makeHtml(`---\n${slides[0].content}\n---`);
-    return Object.freeze(metadataConverter.getMetadata());
-}());
+const metadataConverter = new showdown.Converter({ metadata: true });
 
-slides = slides.slice(1);
+let metadata = parseMetadata();
 
-const title = metadata.title || 'Untitled';
-const lang = metadata.lang || 'en';
-const outputDir = options.output;
-const slideWidth = metadata['slide-width'] || '50%';
-const fontSize = metadata['font-size'] || '28px';
-const fontFamily = metadata['font-family'] || 'Arial, Helvetica, sans-serif';
+function parseMetadata() {
+    console.info('parsing metadata');
 
-console.info('metadata:', JSON.stringify(metadata, null, 4));
+    const metadata = (function() {
+        metadataConverter.makeHtml(`---\n${slides[0].content}\n---`);
+        return Object.freeze(metadataConverter.getMetadata());
+    }());
+
+    slides = slides.slice(1);
+
+    console.info('metadata:', JSON.stringify(metadata, null, 4));
+
+    return metadata;
+}
 
 const MODULES_DIR = path.resolve(path.join(__dirname, '..', 'node_modules'));
-const BUNDLE_DIR = p(process.cwd(), outputDir);
+const BUNDLE_DIR = path.join(process.cwd(), options.output);
 const BUNDLE_RESOURCES = 'resources';
-const BUNDLE_RESOURCES_DIR = p(BUNDLE_DIR, BUNDLE_RESOURCES);
-const PLUGINS_DIR = p(__dirname, 'plugins');
+const BUNDLE_RESOURCES_DIR = path.join(BUNDLE_DIR, BUNDLE_RESOURCES);
+const PLUGINS_DIR = path.join(__dirname, 'plugins');
 const PHASE = {
     RESOURCE: 'resource',
     EXTEND: 'extend',
@@ -93,7 +96,7 @@ console.info('creating bundle directories');
 fs.removeSync(BUNDLE_DIR);
 fs.mkdirSync(BUNDLE_DIR, 0o755);
 fs.mkdirSync(BUNDLE_RESOURCES_DIR, 0o755);
-fs.writeFileSync(p(BUNDLE_DIR, '.gitignore'), '*');
+fs.writeFileSync(path.join(BUNDLE_DIR, '.gitignore'), '*');
 
 const styleSheets = [];
 const javaScripts = [];
@@ -116,7 +119,7 @@ const showdownPlugins = require('./plugins.json').reduce((plugins, name) => {
         createSlide,
     };
 
-    const pluginPath = p(PLUGINS_DIR, name);
+    const pluginPath = path.join(PLUGINS_DIR, name);
     const plugin = require(pluginPath)(metadata, utils);
 
     if (plugin.resources) {
@@ -180,102 +183,123 @@ const showdownPlugins = require('./plugins.json').reduce((plugins, name) => {
     return plugins;
 }, []);
 
-if (extendPlugins.length) {
-    console.info('extending slides');
-    slides = extendPlugins.reduce((slides, plugin) => plugin.run(slides) || slides, slides);
-}
+const converter = new showdown.Converter(Object.assign(require('./config.json'), {
+    extensions: showdownPlugins,
+}));
+converter.setFlavor('github');
 
-console.info('parsing slides');
+build();
 
-let html = (function() {
-    const converter = new showdown.Converter(Object.assign(require('./config.json'), {
-        extensions: showdownPlugins,
-    }));
-    converter.setFlavor('github');
-
-    return slides.reduce((acc, slide) => {
-        const subslide = slide.metadata.get('subslide') ? ' subslide': '';
-        return acc + `<div class="slide${subslide}">\n${converter.makeHtml(slide.content)}\n</div>`;
-    }, '');
-}());
-
-console.info('copying image resources');
-
-html = (function() {
-    const re = /<img (.*?)src[/s]*=[/s]*"(?!http)(.*?)"/mg;
-    return html.replace(re, (match, p1, p2) => {
-        const url = p(BUNDLE_RESOURCES, p2);
-        fs.copySync(p2, p(BUNDLE_RESOURCES_DIR, p2));
-        return `<img ${p1}src="${url}"`;
+if (options.watch) {
+    fs.watchFile(input, () => {
+        console.info('input file updated');
+        timer = process.hrtime();
+        slides = parseInput();
+        metadata = parseMetadata();
+        build();
     });
-}());
-
-console.info('generating output');
-
-const htmlMetaKeys = new Set(['author', 'keywords', 'description']);
-const htmlMeta = Object.entries(metadata).reduce((acc, [key, val]) => htmlMetaKeys.has(key)
-    ? acc + `<meta name="${key}" content="${val}">`
-    : acc,
-'');
-const css = styleSheets.reduce((acc, sheet) => {
-    const css = p(BUNDLE_RESOURCES, sheet.pluginName, sheet.path);
-    return acc + `<link rel="stylesheet" href="${css}">`;
-}, '');
-const scripts = javaScripts.reduce((acc, script) => {
-    const js = p(BUNDLE_RESOURCES, script.pluginName, script.path);
-    return acc + `<script type="text/javascript" src="${js}"></script>`;
-}, '');
-
-const template = `
-<!DOCTYPE html>
-<html lang="${lang}">
-<head>
-    <title>${title}</title>
-    <meta charset="UTF-8">
-    ${htmlMeta}
-    ${css}
-    <style>
-        html {
-            font-size: ${fontSize};
-            font-family: ${fontFamily};
-        }
-        div.slide {
-            width: ${slideWidth};
-        }
-    </style>
-    ${scripts}
-</head>
-<body>
-    <main>
-        ${html}
-    </main>
-</body>
-</html>
-`;
-
-console.info('populating bundle');
-
-const outputHtml = p(BUNDLE_DIR, 'index.html');
-
-fs.writeFileSync(outputHtml, template);
-
-for (let resource of styleSheets.concat(javaScripts, otherResources)) {
-    if (resource.lookup) {
-        fs.copySync(
-            p(resource.pluginPath, resource.lookup),
-            p(BUNDLE_RESOURCES_DIR, resource.pluginName)
-        );
-    } else {
-        fs.copySync(
-            p(resource.pluginPath, resource.path),
-            p(BUNDLE_RESOURCES_DIR, resource.pluginName, resource.path)
-        );
-    }
 }
 
-console.info('presentation:', outputHtml);
+function build() {
+    const title = metadata.title || 'Untitled';
+    const lang = metadata.lang || 'en';
+    const slideWidth = metadata['slide-width'] || '50%';
+    const fontSize = metadata['font-size'] || '28px';
+    const fontFamily = metadata['font-family'] || 'Arial, Helvetica, sans-serif';
 
-const diff = process.hrtime(timer);
-const sec = (diff[0] * 1e9 + diff[1]) / 1e9;
+    if (extendPlugins.length) {
+        console.info('extending slides');
+        slides = extendPlugins.reduce((slides, plugin) => plugin.run(slides) || slides, slides);
+    }
 
-console.info(`done in: ${sec} seconds`);
+    console.info('parsing slides');
+
+    let html = (function() {
+
+        return slides.reduce((acc, slide) => {
+            const subslide = slide.metadata.get('subslide') ? ' subslide': '';
+            return acc + `<div class="slide${subslide}">\n${converter.makeHtml(slide.content)}\n</div>`;
+        }, '');
+    }());
+
+    console.info('copying image resources');
+
+    html = (function() {
+        const re = /<img (.*?)src[/s]*=[/s]*"(?!http)(.*?)"/mg;
+        return html.replace(re, (match, p1, p2) => {
+            const url = path.join(BUNDLE_RESOURCES, p2);
+            fs.copySync(p2, path.join(BUNDLE_RESOURCES_DIR, p2));
+            return `<img ${p1}src="${url}"`;
+        });
+    }());
+
+    console.info('generating output');
+
+    const htmlMetaKeys = new Set(['author', 'keywords', 'description']);
+    const htmlMeta = Object.entries(metadata).reduce((acc, [key, val]) => htmlMetaKeys.has(key)
+        ? acc + `<meta name="${key}" content="${val}">`
+        : acc,
+    '');
+    const css = styleSheets.reduce((acc, sheet) => {
+        const css = path.join(BUNDLE_RESOURCES, sheet.pluginName, sheet.path);
+        return acc + `<link rel="stylesheet" href="${css}">`;
+    }, '');
+    const scripts = javaScripts.reduce((acc, script) => {
+        const js = path.join(BUNDLE_RESOURCES, script.pluginName, script.path);
+        return acc + `<script type="text/javascript" src="${js}"></script>`;
+    }, '');
+
+    const template = `
+    <!DOCTYPE html>
+    <html lang="${lang}">
+    <head>
+        <title>${title}</title>
+        <meta charset="UTF-8">
+        ${htmlMeta}
+        ${css}
+        <style>
+            html {
+                font-size: ${fontSize};
+                font-family: ${fontFamily};
+            }
+            div.slide {
+                width: ${slideWidth};
+            }
+        </style>
+        ${scripts}
+    </head>
+    <body>
+        <main>
+            ${html}
+        </main>
+    </body>
+    </html>
+    `;
+
+    console.info('populating bundle');
+
+    const outputHtml = path.join(BUNDLE_DIR, 'index.html');
+
+    fs.writeFileSync(outputHtml, template);
+
+    for (let resource of styleSheets.concat(javaScripts, otherResources)) {
+        if (resource.lookup) {
+            fs.copySync(
+                path.join(resource.pluginPath, resource.lookup),
+                path.join(BUNDLE_RESOURCES_DIR, resource.pluginName)
+            );
+        } else {
+            fs.copySync(
+                path.join(resource.pluginPath, resource.path),
+                path.join(BUNDLE_RESOURCES_DIR, resource.pluginName, resource.path)
+            );
+        }
+    }
+
+    console.info('presentation:', outputHtml);
+
+    const diff = process.hrtime(timer);
+    const sec = (diff[0] * 1e9 + diff[1]) / 1e9;
+
+    console.info(`done in: ${sec} seconds\n`);
+}
