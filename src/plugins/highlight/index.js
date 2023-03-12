c1b5428afcba11a80e3b6bba0742052be04005b7
Nikola Stoyanov <nikola.da.stoyanov@gmail.com>
2023-03-12 23:05:18 +0200

    Revert "feature(plugins): Add code highlight using tree-sitter"
    This reverts commit 0cdb03668839956cc3b445d1b24f2bf29d1d2394.

--- src/plugins/highlight/index.js
+++ src/plugins/highlight/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
 const hljs = require('highlight.js');
-const tshl = require('tree-sitter-hl');
 
 // Prevent double escape from showdown + hljs
 function unescape(code) {
@@ -13,30 +12,18 @@ function unescape(code) {
 
 module.exports = (metadata, utils) => {
     const css = `${metadata['code-theme'] || 'github'}.css`;
-
-    const hljsFile = path.join(utils.MODULES_DIR, path.join('highlight.js', 'styles', css));
-    const hljsTheme = `hljs-${css}`;
-    fs.copySync(hljsFile, path.join(__dirname, 'dist', hljsTheme));
-
-    const tshlFile = path.join(utils.MODULES_DIR, path.join('tree-sitter-hl', 'styles', css));
-    const tshlTheme = `tshl-${css}`;
-    fs.copySync(tshlFile, path.join(__dirname, 'dist', tshlTheme));
-
-    const tsHighlighter = new tshl.Highlighter();
+    const file = path.join(utils.MODULES_DIR, path.join('highlight.js', 'styles', css));
+    fs.copySync(file, path.join(__dirname, 'dist', css));
 
     return {
-        resources: ['style.css', hljsTheme, tshlTheme],
+        resources: [css],
         after: {
             pattern: /<pre><code class="(.*?)">([\s\S]*?)<\/code><\/pre>/gm,
             replace(_, meta, code) {
                 const lang = meta.split(' ')[0];
 
-                if (lang) {
-                    if (tsHighlighter.supportedLanguages().includes(lang)) {
-                        code = tsHighlighter.highlight(lang, unescape(code)).trim();
-                    } else if (hljs.getLanguage(lang)) {
-                        code = hljs.highlight(lang, unescape(code)).value;
-                    }
+                if (lang && hljs.getLanguage(lang)) {
+                    code = hljs.highlight(lang, unescape(code)).value;
                 }
 
                 return `<pre><code class="${lang}${meta} hljs">${code}</code></pre>`;
